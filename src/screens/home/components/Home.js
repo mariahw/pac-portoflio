bce43badac3e6a5959320254a0baa4dc7f6a9def
Mariah Weathersby <mariahweathersby@gmail.com>
2017-11-19 03:08:10 -0500

    Update style structures

--- src/screens/home/components/Home.js
+++ src/screens/home/components/Home.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 
-import Nav from '../../../common/Nav/components/nav.js'
+import GameBoard from '../../gameBoard/components/GameBoard.js'
 
 const Home = () => (
-  <main>
-    <Nav />
-  </main>
+
+  <div id="main-menu">
+
+      <div id="menu-wrapper" class="dotted absmid">
+
+          <div id="header-logo"></div>
+
+          <ul id="int-main-menu">
+              <li class="btn" id="lvl-1-start">
+                  <Link to="/play"><h3>Play</h3></Link>
+              </li>
+              <li class="btn" id="contact">
+                  <a href="mailto:mariahweathersby@gmail.com"><h3>Contact</h3></a>
+              </li>
+              <li class="btn" id="resume">
+                  <a href="MariahWeathersby-Resume.pdf" download><h3>Resume</h3></a>
+              </li>
+          </ul>
+
+          <div id="credits">
+              <p>Est Nov-1990</p>
+              <p> Pasadena, CALIFORNIA</p>
+              <p>mariahweathersby@gmail.com</p>
+          </div>
+
+      </div>
+
+  </div>
+
 )
 
 export default Home;
