0cc8b7515e763f998f0c94e4c69d223593c25b32
Mariah Weathersby <mariahweathersby@gmail.com>
2017-11-21 14:23:53 -0500

    updated reducer action emitters

--- src/common/HelperModal/reducer.js
+++ src/common/HelperModal/reducer.js
@@ -8,6 +8,19 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    
+    case types.ACTIVATE_HELPER:
+      return Object.assign({}, state, { slideActive: action.payload });
+      break;
+
+    case types.DEACTIVATE_HELPER:
+      return Object.assign({}, state, { slideActive: action.payload })
+      break;
+
+    case types.UPDATE_HELPER_SLIDE:
+      return Object.assign({}, state, { currentSlide: action.payload })
+      break;
+
     default:
       return state;
   }
