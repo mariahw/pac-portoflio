8f64330ad37a1f91f74ecfc575dc3d979cd8bf21
Mariah Weathersby <mariahweathersby@gmail.com>
2017-11-27 12:23:30 -0500

    Import plugins and modify README

--- src/screens/gameBoard/components/GameBoardRender.js
+++ src/screens/gameBoard/components/GameBoardRender.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Script from 'react-load-script'
 
 import Helper from '../../../common/HelperModal/components/HelperModalContainer.js'
 
@@ -25,6 +26,25 @@ const GameBoardRender = () => (
               </div>
           </div>
       </div>
+
+      {/* Libraries */}
+      <Script url="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" />
+
+      <Script url="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js" />
+      <Script url="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js" />
+
+      <Script url="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.min.js" />
+      <Script url="http://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/MorphSVGPlugin.min.js?v=0.6.0" />
+      <Script url="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/plugins/ScrollToPlugin.min.js" />
+
+      <Script url="https://cdnjs.cloudflare.com/ajax/libs/phaser/2.6.2/phaser.min.js" />
+      <Script url="https://cdnjs.cloudflare.com/ajax/libs/swipe/2.0.0/swipe.min.js" />
+
+      <Script url="http://play.mariahweathersby.com/scripts/script.js" />
+
+
+      {/* Game Logic */}
+
   </div>
 
 )
